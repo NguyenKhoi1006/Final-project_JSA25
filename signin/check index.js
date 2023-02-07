3c4cfe7a8af9aeaf44faa2346e06bfec442c87e0
Khôi Nguyễn Đình <nguyendinhkhoi1006@gmail.com>
2023-02-07 21:40:52 +0700

    ver2.5

--- signin/check index.js
+++ signin/check index.js
@@ -1,31 +1,13 @@
-let users = [
-    {
-        user_name: 'admin', 
-        password: 'admin', 
-    },
-    {
-        user_name: 'user', 
-        password: 'user',
-    },
-    {
-        user_name: 'guest', 
-        password: 'guest',
-    },
-]
-for (let i = 0; i < users.length; i++){
-    localStorage.setItem("account_name"+i, users[i].user_name);
-    localStorage.setItem("account_password"+i, users[i].password);
-}
 
 function myFunction(event) {
 var username = document.getElementById("username").value;
 var password = document.getElementById("password").value;
 
 
-for(let i = 0; i < users.length; i++){
-if(username== localStorage.getItem("account_name"+i)&&password== localStorage.getItem("account_password"+i)){
+
+if(username== localStorage.getItem(username)&&password== localStorage.getItem(password)){
     button.action='../index.html';
-    break;
+    
 }
 else  {
     alert("Sai tài khoản mật khẩu");
@@ -34,21 +16,22 @@ else  {
     if (person == null || person == "") {
         alert("Please enter the prompt again");
         event.preventDefault();
-        break
+        
       
     } 
     if (person == "No") {
         button.action='sign-up.html';
-        break
+        
+        
     }
     else {
       
     }
     event.preventDefault();
-    break;
+    
     
 }
 }
-}
+
 var button = document.getElementById("form");
 button.addEventListener('submit',myFunction);
